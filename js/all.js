6257b93b762ae4ad1aeceb1421acee0ea519580d
WinnieWu <tzuhusan1999@gmail.com>
2022-07-20 18:06:19 +0800

    新增環境

---
+++ js/all.js
@@ -0,0 +1,38 @@
+//scrollTop
+$("document").ready(function () {
+  $("a").click(function (event) {
+    event.preventDefault();
+    $("html,body").animate({ scrollTop: 0 }, 1000);
+  });
+  //lightbox
+  lightbox.option({
+    resizeDuration: 200,
+    wrapAround: true,
+  });
+  //swiper
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "vertical",
+    loop: true,
+    autoplay: {
+      delay: 6000,
+    },
+    direction:'horizontal',
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+});
